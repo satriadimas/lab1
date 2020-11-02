185583f8bc53ed5bf8f75c5c786c4d0e0cf9dd6a
satria dimas p <satriadimasp@gmail.com>
2020-11-02 09:07:57 +0700

    input readme.md

--- lab1.py
+++ lab1.py
@@ -12,7 +12,7 @@ print('Z')
 w, x, y, z = 10, 15, 20, 25
 print(w, x, y, z)
 print(w, x, y, z, sep=',')
-print(w, x, y, z, sep='')
+print(w, x, y, z, sep=' ')
 print(w, x, y, z, sep=':')
 print(w, x, y, z, sep='-----')
 
